bf31acbd835164778f8ba35d65504947a61c1d4c
Karina Rabcovskaya <karina6918538@gmail.com>
2022-01-31 09:25:52 +0300

    first commit

---
+++ todo/notes/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+from notes.forms import AddNoteForm
+from notes.models import Note
+
+def index(request):
+   return HttpResponse("Notes index view")
+
+
+
+
+def index(request):
+   if request.method == "POST":
+       form = AddNoteForm(request.POST)
+       if form.is_valid():
+           Note.objects.create(
+               title=form.cleaned_data["title"], text=form.cleaned_data["text"]
+           )
+   else:
+       form = AddNoteForm()
+   notes = Note.objects.all()
+   return render(request, "note_list.html", {"notes": notes, "form": form})
+
